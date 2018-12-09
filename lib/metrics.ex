38fa8e2cd87aa27ddc816a2890b34a64e6ddcdfb
Aditi Malladi <aditimalladi@gmail.com>
2018-12-09 14:45:15 -0500

    Added file metrics

---
+++ lib/metrics.ex
@@ -0,0 +1,51 @@
+defmodule Metrics do
+  use GenServer
+
+  def start_link(args, opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def total_txn(server) do
+    GenServer.call(server, {:total_txn})
+  end
+
+  def btc_mined(server) do
+    GenServer.call(server, {:btc_mined})
+  end
+
+  def total_btc_tx(server) do
+    GenServer.call(server, {:total_btc_tx})
+  end 
+
+  def blockchain_length(server) do
+    GenServer.call(server, {:blockchain_length})
+  end 
+
+
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  def handle_call({:total_txn}, _from, state) do
+    [{_, num_txn}] = :ets.lookup(:metrics, :num_txn)
+    {:reply, num_txn, state}
+  end
+
+  def handle_call({:btc_mined}, _from, state) do
+    [{_, num_btc}] = :ets.lookup(:metrics, :num_btc)
+    {:reply, num_btc, state}
+  end
+
+  def handle_call({:total_btc_tx}, _from, state) do
+    [{_, num_btc_tx}] = :ets.lookup(:metrics, :num_btc_tx)
+    {:reply, num_btc_tx, state}
+  end
+
+  def handle_call({:blockchain_length}, _from, state) do
+    [{_, blockchain_length}] = :ets.lookup(:metrics, :blockchain_length)
+    {:reply, blockchain_length, state}
+  end
+
+
+
+end
